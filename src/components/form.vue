5ffe51eee70571ab7cc008c7b9fbdf3d9a69c599
张志立 <512929087@qq.com>
2023-12-07 21:47:59 +0800

    feat: UI库

---
+++ src/components/form.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <form class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              class="input input-bordered"
+              required
+              v-model.trim.lazy="name"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              class="input input-bordered"
+              required
+              v-model.trim.lazy="pass"
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary" @click="sub">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const pass = ref('')
+const sub = () => {
+  console.log(name.value, pass.value)
+}
+</script>
+
+<style lang="scss" scoped></style>
