fea10c2c9a72d3f77aded43b1987f6cca21fb76b
张志立 <781775743@qq.com>
2023-12-12 17:56:10 +0800

    feat: init

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# 指定基础镜像  
+FROM node:latest as builder
+  
+# 将本地文件复制到镜像中  
+WORKDIR /usr/src/app  
+COPY . .
+# 安装依赖项  
+#RUN npm install  
+
+EXPOSE 1216
+  
+# 设置默认命令  
+CMD [ "node", "app.js" ]
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');  
+const app = express();  
+const port = 1216;  
+const cors = require('cors');  
+app.use(cors({  
+  origin: '*', // 允许来自 example.com 的跨域请求  
+	//  credentials: true // 允许携带凭证 (cookies)  
+  }));
+
+app.get('/welcome', (req, res) => {  
+	  res.send('Hello World!');  
+});  
+  
+app.listen(port, () => {  
+	  console.log(`App listening at http://localhost:${port}`);  
+});
