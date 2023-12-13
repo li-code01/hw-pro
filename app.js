607e82a6581b2827cb12fc92f508d168c96ab13d
张志立 <781775743@qq.com>
2023-12-13 18:05:00 +0800

    feat: 格式化

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');  
-const app = express();  
+const express = require('express');     
+const app = express();
 const port = 1216;  
-const cors = require('cors');  
-app.use(cors({  
+const cors = require('cors');
+app.use(cors({
   origin: '*', // 允许来自 example.com 的跨域请求  
 	//  credentials: true // 允许携带凭证 (cookies)  
   }));
 
 app.get('/welcome', (req, res) => {  
-	  res.send('Hello World!');  
+	  res.send('Hello World!');
 });  
   
 app.listen(port, () => {  
