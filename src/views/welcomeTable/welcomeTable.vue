d3b4a9da5cd35e33fe010a590188419431776118
张志立 <781775743@qq.com>
2024-01-02 18:00:21 +0800

    feat: 开销表格页面

--- src/views/welcomeTable/welcomeTable.vue
+++ src/views/welcomeTable/welcomeTable.vue
@@ -1,41 +1,35 @@
 <template>
   <div class="overflow-x-auto">
-    <table class="table">
-      <!-- head -->
+    <table class="table table-xs">
       <thead>
         <tr>
-          <th></th>
-          <th>Name</th>
-          <th>Job</th>
-          <th>Favorite Color</th>
+          <th>context</th>
+          <th>date</th>
+          <th>mode</th>
+          <th>money</th>
+          <th>pay</th>
+          <th>pay_type</th>
+          <th>pay_user</th>
+          <th>type</th>
+          <th>_id</th>
         </tr>
       </thead>
       <tbody>
-        <break for=""> </break>
-        <!-- row 1 -->
-        <tr class="bg-base-200">
-          <th>1</th>
-
-          <td>Cy Ganderton</td>
-          <td>Quality Control Specialist</td>
-          <td>Blue</td>
-        </tr>
-        <!-- row 2 -->
-        <tr>
-          <th>2</th>
-          <td>Hart Hagerty</td>
-          <td>Desktop Support Technician</td>
-          <td>Purple</td>
-        </tr>
-        <!-- row 3 -->
-        <tr>
-          <th>3</th>
-          <td>Brice Swyre</td>
-          <td>Tax Accountant</td>
-          <td>Red</td>
+        <tr v-for="item in tableData" :key="item._id">
+          <td>{{ item.context }}</td>
+          <td>{{ item.date }}</td>
+          <td>{{ item.mode }}</td>
+          <td>{{ item.money }}</td>
+          <td>{{ item.pay }}</td>
+          <td>{{ item.pay_type }}</td>
+          <td>{{ item.pay_user }}</td>
+          <td>{{ item.type }}</td>
+          <td>{{ item._id }}</td>
         </tr>
       </tbody>
     </table>
+    <button class="join-item btn btn-outline" @click="prevPage">Prev</button>
+    <button class="join-item btn btn-outline" @click="nextPage">Next</button>
   </div>
 </template>
 
@@ -43,11 +37,28 @@
 import { ref, onMounted } from 'vue'
 import service from '@/service/request'
 const tableData = ref([])
+const loading = ref(false)
+const page = ref(1)
+const limit = ref(30)
 const initData = async () => {
-  const res = await service.get('/spendApi?type=edit')
-  tableData.value = res.data
+  const res = await service.get(`/spendApi?type=edit&page=${page.value}&limit=${limit.value}`)
+  console.log('res', res.data.data)
+  tableData.value = res.data.data
 }
-
+const nextPage = () => {
+  console.log('nextPage')
+  if (loading.value === true) return // 加载中时不允许翻页
+  if (tableData.value.length === 0) return // 没有数据时不允许翻页
+  page.value++
+  initData() // 加载上一页数据
+}
+const prevPage = () => {
+  console.log('prevPage')
+  if (loading.value === true) return // 加载中时不允许翻页
+  if (tableData.value.length === 0) return // 没有数据时不允许翻页
+  page.value--
+  initData() // 加载上一页数据
+}
 onMounted(() => {
   // 在组件挂载后执行的代码
   console.log('组件已挂载')
