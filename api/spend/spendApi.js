0c1ea4c3bef7ff2404ab8449bd22c54c116077e1
张志立 <781775743@qq.com>
2024-01-02 18:00:48 +0800

    feat: 开销表格接口

--- api/spend/spendApi.js
+++ api/spend/spendApi.js
@@ -2,13 +2,18 @@ const urlib = require("url");
 const spendMongo = require("../../mongo/spendMongo");
 const { getParamsByUrl } = require("../../utils/paramformat");
 module.exports = async (req, res) => {
-  let { type } = await getParamsByUrl(req);
+  let { type, page, limit } = await getParamsByUrl(req);
   console.log("123", type);
   try {
-    const data = await spendMongo.query("spend_list", {});
+    const data = await spendMongo.queryByPage(
+      "spend_list",
+      {},
+      page,
+      limit - 0
+    );
     res.json({ code: 200, data: data });
   } catch (error) {
     console.log(error);
-    res.json({ code: -200, errmsg: error });
+    res.json({ code: -200, errmsg: error, data: [] });
   }
 };
--- mongo/spendMongo.js
+++ mongo/spendMongo.js
@@ -24,6 +24,24 @@ module.exports = {
   },
 
   /**
+   * 分页查询
+   * @param dbName 数据库名称
+   * @param params 查询条件
+   * @param page  页数
+   * @param limit  每页显示多少条
+   */
+  queryByPage: async function (dbName, params, page, limit) {
+    const db = await mongodb.conn(collectionName);
+    const data = await db
+      .collection(dbName)
+      .find(params)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .toArray();
+    return data;
+  },
+
+  /**
    * 插入一条数据
    * @param dbName 数据库名称
    * @param obj 数据
