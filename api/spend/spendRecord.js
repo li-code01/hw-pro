831477614edd2bb14b2b113ab2f126bda3682964
张志立 <781775743@qq.com>
2024-01-12 16:38:56 +0800

    feat: 增加日期字段

--- api/spend/spendRecord.js
+++ api/spend/spendRecord.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { postParamsByUrl } = require("../../utils/paramformat");
 module.exports = async (req, res) => {
   const postData = req.body;
+  postData.money = Number(postData.money);
   try {
     const data = await spendMongo.insert("spent_list", { ...postData });
     res.json({ code: 200, data: data });
