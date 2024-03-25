fb218461e921961fcf3b5fb4e282c884d4d5e25d
张志立 <781775743@qq.com>
2024-03-25 15:40:21 +0800

    fix🐛: dayjs时间偏差

--- api/gold/index.js
+++ api/gold/index.js
@@ -1,7 +1,11 @@
 const axios = require("axios");
 const dayjs = require("dayjs");
 const goldMongo = require("../../mongo/goldMongo");
+// 获取当前时间的 UTC 时间
+const nowUtc = dayjs.utc();
 
+// 将 UTC 时间转换为北京时间
+const nowBeijing = nowUtc.utcOffset(8);
 // 获取金价
 const getGoldPrice = async () => {
   const data = {
@@ -40,7 +44,7 @@ module.exports = async (req, res) => {
       min: data.min,
       max: data.max,
       heyue: data.heyue,
-      createDate: dayjs().format("YYYY-MM-DD hh:mm:ss"),
+      createDate: nowBeijing.format("YYYY-MM-DD HH:mm:ss"),
     };
     res.json({ code: 200, data: goldInfo });
     recardGoldPrice(goldInfo);
